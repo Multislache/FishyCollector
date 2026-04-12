a51c891fa0f822a41a7e93ceb17c2dad1841ee25
DevShaD0ow <145599061+DevShaD0ow@users.noreply.github.com>
2026-04-12 13:44:34 -0400

    ajout parametre son et sensi
    (pas encore fonctionnel pour la sensi)

--- Source/FishyCollector/Public/FishySaveGame.h
+++ Source/FishyCollector/Public/FishySaveGame.h
@@ -1,42 +1,54 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+    // Fill out your copyright notice in the Description page of Project Settings.
 
-#pragma once
+    #pragma once
 
-#include "CoreMinimal.h"
-#include "GameFramework/SaveGame.h"
-#include "FishInventoryItem.h"
-#include "FishySaveGame.generated.h"
+    #include "CoreMinimal.h"
+    #include "GameFramework/SaveGame.h"
+    #include "FishInventoryItem.h"
+    #include "FishySaveGame.generated.h"
 
-USTRUCT(BlueprintType)
-struct FFishSaveEntry
-{
-    GENERATED_BODY()
+    USTRUCT(BlueprintType)
+    struct FFishSaveEntry
+    {
+        GENERATED_BODY()
 
-    UPROPERTY(SaveGame)
-    FSoftObjectPath FishAssetPath;
+        UPROPERTY(SaveGame)
+        FSoftObjectPath FishAssetPath;
 
-    UPROPERTY(SaveGame)
-    int32 Quantity = 0;
+        UPROPERTY(SaveGame)
+        int32 Quantity = 0;
 
-    UPROPERTY(SaveGame)
-    FFishCatchRecord Capture;
-};
+        UPROPERTY(SaveGame)
+        FFishCatchRecord Capture;
+    };
 
-UCLASS()
-class FISHYCOLLECTOR_API UFishySaveGame : public USaveGame
-{
-    GENERATED_BODY()
+    UCLASS()
+    class FISHYCOLLECTOR_API UFishySaveGame : public USaveGame
+    {
+        GENERATED_BODY()
 
-public:
-    UPROPERTY(SaveGame)
-    TArray<FFishSaveEntry> SavedInventory;
+    public:
+        UPROPERTY(SaveGame)
+        TArray<FFishSaveEntry> SavedInventory;
 
-    UPROPERTY(SaveGame)
-    int32 PlayerMoney;
+        UPROPERTY(SaveGame)
+        int32 PlayerMoney;
 
-    UPROPERTY(SaveGame)
-    TArray<FPrimaryAssetId> OwnedRods;
+        UPROPERTY(SaveGame)
+        TArray<FPrimaryAssetId> OwnedRods;
 
-    UPROPERTY(SaveGame, BlueprintReadWrite, Category = "Save")
-    bool bRodPurchased = false;
-};
+        UPROPERTY(SaveGame, BlueprintReadWrite, Category = "Save")
+        bool bRodPurchased = false;
+
+        UPROPERTY(BlueprintReadWrite, Category = "SaveData")
+        float Sensitivity = 1.0f;
+
+        UPROPERTY(BlueprintReadWrite, Category = "SaveData")
+        float GlobalVolume = 1.0f;
+
+        UPROPERTY(BlueprintReadWrite, Category = "SaveData")
+        float MusicVolume = 0.8f;
+
+        UPROPERTY(BlueprintReadWrite, Category = "SaveData")
+        float SFXVolume = 0.8f;
+    };
